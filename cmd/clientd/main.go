975be90f671f616a11d056fc80519d69443616ce
AlexMapley <alexmapley@gmail.com>
2019-10-06 16:35:43 -0400

    balance reading example

--- cmd/clientd/main.go
+++ cmd/clientd/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -28,15 +29,26 @@ func main() {
 
 	balance, err := client.BalanceAt(context.Background(), account, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 	fmt.Println(balance) // 25893180161173005034
 
-	blockNumber := big.NewInt(5532993)
-	balance, err = client.BalanceAt(context.Background(), account, blockNumber)
+	// blockNumber := big.NewInt(5532993)
+	// balanceAt, err = client.BalanceAt(context.Background(), account, blockNumber)
+	balanceAt, err := client.BalanceAt(context.Background(), account, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 	fmt.Println(balance) // 25729324269165216042
+
+	fbalance := new(big.Float)
+	fbalance.SetString(balanceAt.String())
+	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+	fmt.Println(ethValue) // 25.729324269165216041
+
+	pendingBalance, err := client.PendingBalanceAt(context.Background(), account)
+	fmt.Println(pendingBalance) // 25729324269165216042
+
+	fmt.Println(ethValue) // 25.729324269165216041
 
 }
